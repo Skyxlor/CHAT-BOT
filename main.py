d72693a8cbe93d4dfb9926018d5cba4393ba616a
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-10-03 21:14:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import re
 import asyncio
 import time
 from datetime import datetime
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from helper_func import subscribed, encode, decode, get_messages
 #from database.users_chats_db import db
 
 API_ID = "28630777"
@@ -64,7 +66,42 @@ async def restart(client, m: Message):
     await accha.edit("╔═══❰𝐖𝐄𝐋𝐂𝐎𝐌𝐄❱═══❍⊱❁۪۪\n║\n║➣ @r3Ady_4_m3\n║\n║➣ @OFFIC3AL_N3TWORK\n║\n║➣\n║\n╚══════ஜ۩۞۩ஜ═════╝")
     await asyncio.sleep(0.2)
     await accha.edit("╔═══❰𝐖𝐄𝐋𝐂𝐎𝐌𝐄❱═══❍⊱❁۪۪\n║\n║➣ @r3Ady_4_m3\n║\n║➣ @OFFIC3AL_N3TWORK\n║\n║➣ @OFFIC3AL_BOT\n║\n╚══════ஜ۩۞۩ஜ═════╝")
-     
+
+
+@Bot.on_message(filters.command('start') & filters.private)
+async def not_joined(client: Client, message: Message):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                "Join Channel",
+                url = client.invitelink)
+        ]
+    ]
+    try:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
+        )
+    except IndexError:
+        pass
+
+    await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+    )
+
 
 @bot.on_message(
     filters.command(["chatbot off", f"chatbot@{BOT_USERNAME} off"], prefixes=["/", ".", "?", "-"])
